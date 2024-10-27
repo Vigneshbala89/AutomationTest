5d405fe8fc0a57dc2d4775ff05debca5d2f70701
7702 <7702@TNQ-LTP-715.tnq.co.in>
2024-10-27 10:22:09 +0530

    Add new test script

---
+++ advance-selenium9-1/src/test/java/testpackage/DemoTest.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void vtest()
+	{
+		Reporter.log("Vignesh", true);
+	}
+
+}
