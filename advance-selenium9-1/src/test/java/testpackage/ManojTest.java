e26263d65195acdfd2360dc1af778e8f4b7c8fee
manug <manug@Manoj_Kumar_M.mshome.net>
2024-10-27 10:21:46 +0530

    Added New Test Script

---
+++ advance-selenium9-1/src/test/java/testpackage/ManojTest.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ManojTest {
+
+	@Test
+	public void manojTest() {
+		Reporter.log("Manoj", true);
+	}
+}
