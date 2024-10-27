a53dab02750474f6b9548096323009141ac32cd3
LENOVO <LENOVO@DESKTOP-QGRVUKK>
2024-10-27 10:35:10 +0530

    Added NewTest Script

---
+++ advance-selenium9-1/src/test/java/testpackage/VigneshTest.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class VigneshTest {
+	@Test
+	public void vigneshTest() {
+		Reporter.log("Vignesh",true);
+	}
+
+}
