9df5881d5d5a7144a47a5b07736c832fa4c836ce
LENOVO <LENOVO@DESKTOP-QGRVUKK>
2024-11-06 14:17:11 +0530

    New Testcase added

---
+++ advance-selenium9-1/src/test/java/testpackage/suriyaTest.java
@@ -0,0 +1,14 @@
+package testpackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class suriyaTest {
+
+	
+	@Test
+	public void testCase() {
+		
+		Reporter.log("Suriya Test Case",true);
+	}
+}
